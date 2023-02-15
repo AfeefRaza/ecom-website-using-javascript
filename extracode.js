58ddecede0ce4e4829caff9bbbe97ac9dcb24625
afeef raza <afeefraza691@gmail.com>
2023-02-15 18:46:39 +0500

    6th commit
    categories images and other pages were  not showing is fixed

---
+++ extracode.js
@@ -0,0 +1,51 @@
+// import categories from "./categories.js";
+// if (page !== "index" && page !== "") {
+  
+// } else {
+//   let category = categories;
+// document.getElementById("cat").innerHTML = category.map(cat => {
+//   return `
+    
+//   `;
+// }).join("");
+
+// window.onload = function() {
+//   document.querySelectorAll(".cat").forEach(el => {
+//     el.addEventListener("click", e => {
+//       const clickedCatElement = e.target.closest(".cat");
+//       if (clickedCatElement) {
+//         const clickedCat = categories.find(cat => cat.name === clickedCatElement.querySelector("h1").innerText);
+//         if (clickedCat) {
+//           console.log(clickedCat.slug);
+//         } else {
+//           console.error("No matching category found");
+//         }
+//       } else {
+//         console.error("No .cat element found");
+//       }
+//     });
+//   });
+// };
+// }
+
+
+
+    // document.getElementById('cat').innerHTML = category.map(cat => 
+    //    catslug = cat.slug
+    //     `
+    //     <a href="/${cat.slug}.html">
+    //     <div  class="cat">
+          
+    //       <h1>${cat.name}</h1>
+    //       <div class="image">
+    //       <img src=${cat.image} alt="">
+    //       </div>
+          
+    //     </div>
+    //     </a>
+    //     `
+       
+    // ).join('')
+    
+    // Get the current URL
+
--- javascript.js
+++ javascript.js
@@ -1,54 +1,3 @@
-// import categories from "./categories.js";
-// if (page !== "index" && page !== "") {
-  
-// } else {
-//   let category = categories;
-// document.getElementById("cat").innerHTML = category.map(cat => {
-//   return `
-    
-//   `;
-// }).join("");
-
-// window.onload = function() {
-//   document.querySelectorAll(".cat").forEach(el => {
-//     el.addEventListener("click", e => {
-//       const clickedCatElement = e.target.closest(".cat");
-//       if (clickedCatElement) {
-//         const clickedCat = categories.find(cat => cat.name === clickedCatElement.querySelector("h1").innerText);
-//         if (clickedCat) {
-//           console.log(clickedCat.slug);
-//         } else {
-//           console.error("No matching category found");
-//         }
-//       } else {
-//         console.error("No .cat element found");
-//       }
-//     });
-//   });
-// };
-// }
-
-
-
-    // document.getElementById('cat').innerHTML = category.map(cat => 
-    //    catslug = cat.slug
-    //     `
-    //     <a href="/${cat.slug}.html">
-    //     <div  class="cat">
-          
-    //       <h1>${cat.name}</h1>
-    //       <div class="image">
-    //       <img src=${cat.image} alt="">
-    //       </div>
-          
-    //     </div>
-    //     </a>
-    //     `
-       
-    // ).join('')
-    
-    // Get the current URL
-
 
     import Allproducts from "./items.js"
     import { electronics, women, men, jewelery } from "./categoryproducts.js";
@@ -99,7 +48,13 @@
         <a href="#" class="btn"> Add to Cart</a>
       </div>`
     ).join('');
-    
+
+    // var menu = document.querySelector('.cartmenu') // Using a class instead, see note below.
+
+
+    // function cart() {
+    //   menu.classList.toggle('show');
+    // }
 
 
 
